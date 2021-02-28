2d402d9db5ca623cca4dcc6e18225c5e9cc79ac4
GulArmagan <g.armagan.kurtoglu@gmail.com>
2021-02-28 18:39:22 +0300

    ilk commit master branch ten

--- src/git_proje02/Class1.java
+++ src/git_proje02/Class1.java
@@ -3,7 +3,7 @@ package git_proje02;
 public class Class1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("ilk commit local masterdan");
 
 	}
 
