70a85aa0347f4089483e57eb07cc2fdacd5ae945
GulArmagan <67240356+GulArmagan@users.noreply.github.com>
2021-02-28 19:33:20 +0300

    Update Class1.java

--- src/git_proje02/Class1.java
+++ src/git_proje02/Class1.java
@@ -4,8 +4,8 @@ public class Class1 {
 
 	public static void main(String[] args) {
 		System.out.println("ilk commit local masterdan");
- feature
-		System.out.println("ilk commit local masterdan"); master
+		
+		System.out.println("ilk commit local masterdan"); 
 	}
 
 }
